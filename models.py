20d31de95dc8f472b60c5beaf6cfd10163a275f2
LemonClubCollective <lemonclub@usa.com>
2025-07-23 08:46:12 +0700

    Fixed User model for Flask-Login with UserMixin

--- models.py
+++ models.py
@@ -1,10 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
